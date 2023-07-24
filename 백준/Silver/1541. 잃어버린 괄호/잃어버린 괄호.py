0dac90464135e6d6243c6060ab6ca9790cc795f5
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-07-24 16:52:36 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,31 +1,14 @@
-input = input()
-ans = 0
-temp = 0
-flag = True
-for i in range(len(input)):
-    c = input[i]
-    if c != '-' and c != '+':
-        temp *= 10
-        temp += int(c)
-    if c == '+':
-        if flag:
-            ans += temp
-            temp = 0
-        else:
-            ans -= temp
-            temp = 0
-    elif c == '-':
-        if flag:
-            ans += temp
-            temp = 0
-            flag = False
-        else:
-            ans -= temp
-            temp = 0
-    if i == len(input) - 1:
-        if flag:
-            ans += temp
-        else:
-            ans -= temp
+expression = input()
+ans, temp, flag = 0, 0, True
+
+for i, c in enumerate(expression):
+    if c.isdigit():
+        temp = temp * 10 + int(c)
+    if c in ('+', '-'):
+        ans = ans + temp if flag else ans - temp
+        temp = 0
+        if c == '-': flag= False
+    if i == len(expression) - 1:
+        ans = ans + temp if flag else ans - temp
 
 print(ans)
