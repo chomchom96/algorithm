d4cbdddae608b85ef2c32f1e66472d22cef4464b
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-07-25 12:03:25 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 2104 ms, Memory: 98800 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int node, edge;
+    static List<List<Integer>> I = new ArrayList<>();
+    static int[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        node = Integer.parseInt(st.nextToken());
+        edge = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+
+        visited = new int[node + 1];
+        for (int i = 0; i <= node; i++) {
+            I.add(new ArrayList<>()); // 이차원 리스트, 그래프 표현
+        }
+
+        for (int i = 0; i < edge; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            I.get(A).add(B);
+            I.get(B).add(A); // graph에 (A,B), (B,A) 추가
+        }
+
+        for (int i = 1; i <= node; i++) {
+            Collections.sort(I.get(i)); // 문제에서 노드 내림차순
+        }
+
+        bfs(start);
+        
+        for (int i = 1; i <= node; i++){
+            System.out.println(visited[i]);
+        }
+
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        int cnt = 1;
+
+        queue.offer(start);
+        visited[start] = cnt++;
+
+        while(!queue.isEmpty()) {
+            int curNode = queue.poll();
+
+            for (int i = 0; i < I.get(curNode).size(); i++) {
+                int nextNode = I.get(curNode).get(i);
+
+                if (visited[nextNode] != 0) {
+                    continue;
+                }
+
+                queue.offer(nextNode);
+                visited[nextNode] = cnt++;
+            }
+        }
+    }
+}
