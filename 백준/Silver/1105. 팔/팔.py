1cb227028bc8989dc49f62b098ea7f1dd1abaac1
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-08-01 09:46:19 +0900

    [Silver I] Title: 팔, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.py
+++ 백준/Silver/1105. 팔/팔.py
@@ -3,15 +3,14 @@ input = sys.stdin.readline
 
 N, M = map(str,input().split())
 
-while True:
-    if len(N) != len(M):
-        print(0)
-        break
-    elif N == M:
-        print(N.count('8'))
-        exit()
-    else:
-        for i in range(len(N)):
-            if N[i] != M[i]:
-                print(N[:i].count('8'))
-                exit()
+if len(N) != len(M):
+    print(0)
+    exit()
+elif N == M:
+    print(N.count('8'))
+    exit()
+else:
+    for i in range(len(N)):
+        if N[i] != M[i]:
+            print(N[:i].count('8'))
+            exit()
