73cbad9670a0daac5b37f11418754ba10753c248
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-08-09 17:16:27 +0900

    [Silver III] Title: 칸토어 집합, Time: 296 ms, Memory: 20416 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -2,20 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.io.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException{
+    static int n;
+    static char c[];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str;
-		while ((str = br.readLine()) != null) {
-			int i = Integer.parseInt(str);
-			StringBuilder sb = new StringBuilder();
-			for (int j = 0; j < Math.pow(3, i); j++) {
-				String tri = Integer.toString(j,3);
-				if (!tri.contains("1")) {
-					sb.append('-');
-				} else sb.append(' ');
-			}	
-			System.out.println(sb);
-		}
-	}
+        while((str = br.readLine()) != null){
+            n = Integer.parseInt(str); //N
+
+            int pow = (int) Math.pow(3,n);
+            c = new char[pow];
+
+            for(int i=0;i<pow;i++) c[i] = '-';
+
+            back(0,pow);
+
+            for(int i=0;i<pow;i++) bw.write(c[i]);
+            bw.write("\n");
+            bw.flush();
+        }
+    }
+
+    static void back(int start,int length){
+        if(length < 3) {
+            return;
+        }
+        for(int i=start+length/3;i<start+length/3*2;i++) c[i] = ' ';
+        back(start,length/3);
+        back(start+length/3*2,length/3);
+    }
 }
