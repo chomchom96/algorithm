ee97ed9b029cdf95bf7c1b020cc2e5ca6bb9d468
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-08-10 09:49:29 +0900

    [Silver I] Title: Z, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -5,29 +5,32 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-	static int n,r,c;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        int cnt  = 0;
-        while (n!=0) { // size 1 까지 탐색
-        	n--;
-        	int s = (int) Math.pow(2, n); // s for size
-        	if (r < s && c < s) continue;
+	static int n,r,c,cnt;
+	
+	 public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       n = Integer.parseInt(st.nextToken());
+       r = Integer.parseInt(st.nextToken());
+       c = Integer.parseInt(st.nextToken());
+       System.out.println(div(r,c));
+	 }
+	 
+	 static int div(int r, int c) {
+		 if (n!=0) {
+			n--;
+			int s = (int) Math.pow(2, n);
+			if (r < s && c < s) div(r,c);
         	else if (r < s && c >= s) {
-        		cnt += s*s; c -= s;
+        		cnt += s*s; div(r,c-s);
         	}
         	else if (c < s && r >= s) {
-        		cnt += 2*s*s; r -= s;
+        		cnt += 2*s*s; div(r-s,c);
         	}
         	else {
-        		cnt += 3*s*s; r -= s; c -= s;
+        		cnt += 3*s*s; div(r-s,c-s);
         	}
-        }
-        System.out.println(cnt);
-    }
+		 }
+		return cnt;
+	 }
 }
