2b4ae67beadd435898f0b207806b9e0f0f89144a
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-09-13 14:35:06 +0900

    [Silver I] Title: 인형들, Time: 304 ms, Memory: 21380 KB -BaekjoonHub

---
+++ 백준/Silver/15954. 인형들/인형들.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+	public static void main(String[] args) throws IOException {
+	    StringTokenizer st;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    st = new StringTokenizer(br.readLine());
+	    int n = Integer.parseInt(st.nextToken());
+	    int K = Integer.parseInt(st.nextToken());
+	    
+	    double[] nums = new double[n];
+	    st = new StringTokenizer(br.readLine());
+	    for (int i = 0; i < n; i++) nums[i] = Double.parseDouble(st.nextToken());
+	    double ans = Double.MAX_VALUE;
+	    for (int k = K; k <= n; k++) {
+		    for (int i = 0; i < n - k + 1; i++) {
+		    	double avg = 0;
+		    	for (int j = i; j < i + k; j++) avg += nums[j];
+		    	avg /= k;
+		    	double dev = 0;
+		    	for (int j = i; j < i + k; j++) dev += Math.pow(nums[j] - avg, 2) / k;
+		    	
+		    	double stdev = Math.sqrt(dev);
+		    	ans = Math.min(ans, stdev);
+		    }
+	    }
+	    System.out.println(String.format("%.11f", ans));
+	}
+}
