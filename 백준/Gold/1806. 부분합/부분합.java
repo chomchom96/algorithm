7174cc7db3c5d43a29aec21d0203fbd8ed4bf10d
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-09-21 14:13:44 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 23552 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,40 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-        	arr[i] = Integer.parseInt(st.nextToken());
-        	if (i > 0) arr[i] += arr[i - 1];
-        }
-                
-        int leftPointer = 0;
-        int len = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-    		if (arr[i] >= s) {len = Math.min(i + 1, len); break;}
-    	}
-        
-        while (leftPointer < n) {
-        	
-        	if (len == 1) break;
-        	
-        	
-        	
-        	for (int i = 1; i < Math.min(len, n - leftPointer); i++) {
-        		if (arr[leftPointer + i] - arr[leftPointer] >= s) 
-        			{len = Math.min(len, i); break;}
-        	}
-        	leftPointer++;
-        }      
-        System.out.println(len == Integer.MAX_VALUE? 0 : len); 
-    }
+
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i =0; i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+
+		int left = 0;
+		int right = 0;
+		int sum = 0;
+		int answer = Integer.MAX_VALUE;
+		while (left <=N && right <=N) {
+			if(sum >= S && answer > right-left) {
+				sum -= arr[left];
+				answer = right-left;
+				left++;
+		
+			}
+			if(sum < S){
+				sum += arr[right++];
+			}
+			else {
+				sum-= arr[left++];
+			}
+		}
+		System.out.println(answer == Integer.MAX_VALUE? 0: answer);
+	}
+
 }
