7e9c07c350535ddc4e6a58f5c2d86fef1f4930a5
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-09-26 15:30:54 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 220 ms, Memory: 15288 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static int MOD = 1000000007;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[n];
+        long[] pows = new long[n];
+        pows[0] = 1L;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+        	arr[i] = Long.parseLong(st.nextToken());
+        	if (i > 0) pows[i] = (pows[i - 1] * 2) % MOD;
+        }
+        
+        Arrays.sort(arr);
+        
+        long sum = 0;
+        for (int i = 0; i < n - 1; i++) for (int j = i + 1; j < n; j++) sum += ((arr[j] - arr[i]) * pows[j - i - 1]) % MOD; 
+
+        System.out.println(sum % MOD); 
+    }
+}
