e8e01d934b0c83976ebaa976928e3777c73bf743
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-10-12 15:25:41 +0900

    [Gold II] Title: 금고 테스트, Time: 248 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Gold/2266. 금고 테스트/금고 테스트.java
+++ 백준/Gold/2266. 금고 테스트/금고 테스트.java
@@ -1,33 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main {
-	
-	static StringTokenizer st;
-	
-	
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	st = new StringTokenizer(br.readLine());
-    	int c = Integer.parseInt(st.nextToken());
-    	int r = Integer.parseInt(st.nextToken());
-    	
-    	int[][] dp = new int[r+1][c+1];
-    	for (int i = 0; i <= c; i++) dp[1][i] = i;
-    	for (int i = 0; i <= r; i++) dp[i][1] = 1;
-    	
-    	for (int i = 2; i <= r; i++) for (int j = 2; j <= c; j++) dp[i][j] = Integer.MAX_VALUE;
-    	
-    	for (int i = 2; i <= r; i++) 
-    		for (int j = 2; j <= c; j++) 
-    			for (int k = 1; k < j; k++) 
-    				dp[i][j] = Math.min(dp[i][j], Math.max(dp[i-1][k-1], dp[i][j-k]) + 1);
-    	
-//    	for (int i = 0; i <= r; i++) System.out.println(Arrays.toString(dp[i]));
-    	System.out.println(dp[r][c]);
-    	
-	}
+import java.util.Scanner;
+ 
+class Main {
+    static int minTrials(int n, int k)
+    {
+        int dp[] = new int[n + 1], m;
+        for (m = 0; dp[n] < k; m++) {
+            for (int x = n; x > 0; x--) {
+                dp[x] += 1 + dp[x - 1];
+            }
+        }
+        return m;
+    }
+ 
+    /* Driver code*/
+    public static void main(String[] args)
+    {	
+    	Scanner sc = new Scanner(System.in);	
+    	int n = sc.nextInt();
+    	int k = sc.nextInt();
+        System.out.println(minTrials(k, n));
+    }
 }
