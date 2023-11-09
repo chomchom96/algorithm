88555baadc0c26d67493cf89d292ab1e9c4e3847
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-11-09 14:05:37 +0900

    [Gold IV] Title: 소수 경로, Time: 160 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Gold/1963. 소수 경로/소수 경로.java
+++ 백준/Gold/1963. 소수 경로/소수 경로.java
@@ -2,15 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.HashSet;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static Set<Integer> sieve = new HashSet();
-	
+	static boolean[] isNotPrime;
 	
 	public static void main(String[] args) throws IOException {
 		sieve();
@@ -27,9 +24,8 @@ public class Main {
 	}
 
 	private static void sieve() {
-		boolean[] isNotPrime = new boolean[10001];
+		isNotPrime = new boolean[10001];
 		for (int i = 2; i <= 100; i++) if (!isNotPrime[i]) for (int j = 2; j <= 10000/i; j++) isNotPrime[i*j] = true;
-		for (int i = 1001; i < 10000; i++) if (!isNotPrime[i]) sieve.add(i);
 	}
 
 	private static void bfs(String a, String b) {
@@ -53,8 +49,7 @@ public class Main {
 				for (int digit = 0; digit < 10; digit++) {
 					if (i == 0 && digit == 0) continue;
 					 int next = num - (nStr.charAt(i) - '0') * (int) Math.pow(10, 3 - i) + digit * (int) Math.pow(10, 3 - i);
-					 if (sieve.contains(next) && !visited[next] && next > 1000) queue.offer(new int[] {next, cnt+1});
-
+					 if (!isNotPrime[next] && !visited[next]) queue.offer(new int[] {next, cnt+1});
 				}
 			}
 		}
