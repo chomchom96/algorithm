0fc6dd639f8799b0fc87ee88a1cd92fb06a5c64b
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2023-11-18 09:38:34 +0900

    [Gold IV] Title: 키 순서, Time: 852 ms, Memory: 38036 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int maxDistance = n * (n * (n - 1) / 2); 
+        
+        int[][] distances = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                distances[i][j] = maxDistance; 
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken()) - 1;
+            int n2 = Integer.parseInt(st.nextToken()) - 1;
+            distances[n1][n2] = 1;
+        }
+        
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (distances[i][j] > distances[i][k] + distances[k][j]) {
+                        distances[i][j] = distances[i][k] + distances[k][j];
+                    }
+                }
+            }
+        }
+        
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (distances[i][j] < maxDistance || distances[j][i] < maxDistance) count++;
+            }
+            if (count == n - 1) result++;
+        }
+        
+        System.out.println(result);
+    }    
+}
