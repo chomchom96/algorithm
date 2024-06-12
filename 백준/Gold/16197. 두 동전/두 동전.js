9d30a19e9bfec8d879b1c13a428971e4af58e760
chomchom96 <112466460+chomchom96@users.noreply.github.com>
2024-06-12 23:53:25 +0900

    [Gold IV] Title: 두 동전, Time: 1524 ms, Memory: 16484 KB -BaekjoonHub

---
+++ 백준/Gold/16197. 두 동전/두 동전.js
@@ -0,0 +1,57 @@
+const sol = (input) => {
+  const [N, M] = input[0].split(" ").map(Number);
+  const board = input.slice(1).map((str) => str.split(""));
+  const coin = [];
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (board[i][j] === "o") coin.push([i, j]);
+    }
+  }
+
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
+  let min = Number.MAX_SAFE_INTEGER;
+
+  function checkDrop(x, y) {
+    if (x < 0 || y < 0 || x >= N || y >= M) return true;
+    return false;
+  }
+
+  function checkWall(x, y, idx) {
+    const [nx, ny] = [x + dx[idx], y + dy[idx]];
+    if (board[nx]) { 
+      if (board[nx][ny] === "#") return [x, y];
+    }
+    return [nx, ny];
+  }
+
+  function dfs(cnt, x1, y1, x2, y2) {
+    if (cnt >= min) return;
+    if (cnt > 10) return;
+    if (checkDrop(x1, y1) && checkDrop(x2, y2)) return;
+    if (checkDrop(x1, y1) || checkDrop(x2, y2)) {
+      min = Math.min(min, cnt);
+      return;
+    }
+    for (let i = 0; i < 4; i++) {
+      const [nx1, ny1] = checkWall(x1, y1, i);
+      const [nx2, ny2] = checkWall(x2, y2, i);
+      dfs(cnt + 1, nx1, ny1, nx2, ny2);
+    }
+  }
+
+  dfs(0, coin[0][0], coin[0][1], coin[1][0], coin[1][1]);
+  if (min === Number.MAX_SAFE_INTEGER) return -1;
+  return min;
+};
+
+const input = [];
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", (line) => {
+    input.push(line);
+  })
+  .on("close", () => {
+    console.log(sol(input));
+    process.exit();
+  });
